7c6ff3173c1b977986eebbb8955d7fc7dbfe346d
Allison Sousa Bahls <allison_sousa_bahls@hotmail.com>
2021-01-05 13:19:53 -0400

    GET /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
@@ -33,7 +33,6 @@ public class Order implements Serializable{
 	@JoinTable(name = "tb_order_product", 
 	joinColumns = @JoinColumn(name="order_id"),
 	inverseJoinColumns = @JoinColumn(name="product_id"))
-	
 	private Set<Product> products = new HashSet<>();
 	
 	public Order() {
