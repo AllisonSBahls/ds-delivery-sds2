a2f853da0b44d69221c63c15dd72f795176f152f
Allison Sousa Bahls <allison_sousa_bahls@hotmail.com>
2021-01-08 14:01:46 -0400

    Search and Location

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,9 +1,25 @@
 import './styles.css'
 import StepsHeader from './StepsHeader';
+import ProductList from './ProductList';
+import { useEffect, useState } from 'react';
+import { Product, OrderLocationData } from './types';
+import { fetchProducts } from '../api';
+import OrderLocation from './OrderLocaltion';
+
 function Orders(){
+    const [products, setProducts] = useState<Product[]>([]);
+    const [orderLocaltion, setOrderLocation] = useState<OrderLocationData>(); 
+
+    useEffect(() => {fetchProducts()
+        .then(response => setProducts(response.data))
+        .catch(error => console.log(error))
+    }, []);
+  
     return (
-      <div className="orders-containers">
+      <div className="orders-container">
           <StepsHeader/>
+          <ProductList products={products}/>
+          <OrderLocation onChangeLocation={location => setOrderLocation(location)}/>
       </div>
     )
 }
